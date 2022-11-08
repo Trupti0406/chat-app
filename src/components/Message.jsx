090b0bcf05913c12a8d921311d7ae015a9983b59
trupti0406 <truptivijayyadav27@gmail.com>
2022-11-08 23:51:52 +0530

    ui pending

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>Message</div>
-  )
+    <div className="message owner">
+      <div className="messageInfo">
+        <img
+          src="https://images.pexels.com/photos/38554/girl-people-landscape-sun-38554.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt="user image"
+        />
+        <span>Just now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello, how are you?</p>
+        {/* <img
+          src="https://images.pexels.com/photos/38554/girl-people-landscape-sun-38554.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt="user image"
+        /> */}
+      </div>
+    </div>
+  );
 }
 
 export default Message
